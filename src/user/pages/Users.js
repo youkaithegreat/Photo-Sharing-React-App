41ae8458fd4edcc9a6dead92e0f1f7a54497c6cd
Kevin Yang <yangkevint@gmail.com>
2022-08-12 09:45:43 -0500

    Cleaning up

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,23 +5,22 @@ import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
+
   const [ isLoading, setIsLoading ] = useState( false );
   const [ error, setError ] = useState();
   const [ loadedUsers, setLoadedUsers ] = useState();
-
 
   useEffect( () => {
     const sendRequest = async () => {
       setIsLoading( true )
       try {
-        const response = await fetch( 'http://localhost:3001/api/users' )
 
+        const response = await fetch( 'http://localhost:3001/api/users' )
         const responseData = await response.json();
 
         if ( !response.ok ) {
           throw new Error( responseData.message );
         }
-
         setLoadedUsers( responseData.users )
 
       } catch ( err ) {
@@ -37,14 +36,12 @@ const Users = () => {
   }
 
   return ( <React.Fragment>
-
     <ErrorModal error={ error } onClear={ errorHandler } />
     { isLoading && ( <div className="center">
       <LoadingSpinner />
     </div> ) }
     { !isLoading && loadedUsers && <UsersList items={ loadedUsers } /> }
   </React.Fragment> )
-
 };
 
 export default Users;
