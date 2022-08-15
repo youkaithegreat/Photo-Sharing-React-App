ea655abe6436ce7aac9b5dfbd6433156b2a976bb
Kevin Yang <yangkevint@gmail.com>
2022-08-15 13:43:58 -0500

    Adding Image Upload

---
+++ src/shared/components/FormElements/ImageUpload.js
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react'
+
+import Button from './Button'
+
+
+import './ImageUpload.css'
+const filePickerRef = useRef();
+
+
+const ImageUpload = props => {
+    const pickImageHandler = () => {
+        filePickerRef.current.click()
+    }
+
+    const pickedHandler = event => {
+        console.log( event.target )
+    }
+
+    return (
+        <div className="form-control">
+            <input
+                id={ props.id }
+                ref={ filePickerRef }
+                style={ { display: 'none' } }
+                type="file"
+                accept=".jpg,.png,.jpeg"
+                onChange={ pickedHandler } />
+            <div className={ `image-upload ${props.center && 'center'}` }>
+                <div className="image-upload__preview">
+                    <img src="" alt="Preview" />
+                </div>
+                <Button type="button" onClick={ pickImageHandler }>Pick Image</Button>
+            </div>
+        </div>
+    )
+}
+
+export default ImageUpload;
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
     VALIDATOR_EMAIL,
     VALIDATOR_MINLENGTH,
@@ -110,6 +111,7 @@ const Auth = () => {
                             onInput={ inputHandler }
                         />
                     ) }
+                    { !isLoginMode && <ImageUpload center id="" /> }
                     <Input
                         element="input"
                         id="email"
